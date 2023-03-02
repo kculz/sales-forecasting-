7142db2a7db54c3411ff7e0dd28fa15ac1d55b2f
kculz <munyamakudzai095@gmail.com>
2023-03-02 09:13:07 +0200

    upload template done

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def signup():
 
 @app.route('/')
 def home():
-    return render_template('login.html')
+    return render_template('upload.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
